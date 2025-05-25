fd1f66547ef5d7a9dbe79f0d63742b87e5416d29
Muhammad Mubasahar <muhammadmubasharoffical@gmail.com>
2025-05-25 21:57:22 +0500

    Update Header.tsx

--- app/components/header/Header.tsx
+++ app/components/header/Header.tsx
@@ -16,12 +16,12 @@ export default function Header() {
 
       <div className="relative z-20 flex flex-col justify-center items-start h-full px-6 md:px-24 max-w-7xl mx-auto -mt-20">
         <p className="inline-block bg-gray-400 text-red-500 text-xs px-3 py-1 rounded-full font-medium">
-          Made in Indonesia, dedicated to Indonesia
+          Made in Indonesia, dedicated to Indonesiai
         </p>
         <h1 className="text-4xl sm:text-5xl md:text-6xl font-serif font-semibold leading-tight text-white max-w-2xl">
           Discover the Art of
           <br />
-          Dressing Up
+          Dressing Ups
         </h1>
         <p className="mt-15 text-sm tracking-widest text-gray-300 uppercase">
           Scroll down ↓
